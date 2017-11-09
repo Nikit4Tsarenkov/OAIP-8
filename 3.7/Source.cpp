1bb4d5b8414232c54342c645e7172d7e8618b8e5
Nikit4Tsarenkov <32556135+Nikit4Tsarenkov@users.noreply.github.com>
2017-11-09 11:55:03 +0200

    Update Source.cpp

--- 3.7/Source.cpp
+++ 3.7/Source.cpp
@@ -1,39 +1,20 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
-void rootCount(int a, int b, int c) {
-	int D = (b*b) - (4 * (a * c));
-	if (D > 0) 
-		cout << 2;
-	else if (D = 0)
-		cout << 1;
-	else
-		cout << 0;
-}
- void main()
-{
-	int a, b, c;
-	float a1, b1, c1;
-	cout << "Input first koef: "; cin >> a;
-	cout << "Input second koef: "; cin >> b;
-	cout << "Input third koef: "; cin >> c;
-	cout << "Input veshest first koef: "; cin >> a1;
-	cout << "Input veshest second koef: "; cin >> b1;
-	cout << "Input veshest third koef: "; cin >> c1;
-	rootCount(a, b, c);
-	rootCount(a1, b1, c1);
+
+int main(){
+	int number, a = 0;
+	cout << "input number"; cin >> number;
+			while (number % 10 == 0){
+		number /= 10;
+	}
+	while (number != 0)
+	{
+		if (number % 10 == 0)
+			a++;
+		number /= 10;
+	}
+	cout << a << endl;
 	system("pause");
+	return 0;
 }
-
- double rootCount(float a1, float b1, float c1) {
-	 float D = (b1*b1) - (4 * (a1 * c1));
-	 if (D > 0)
-		 return 2;
-	 else if (D = 0)
-		 return 1;
-	 else
-		 return 0;
- }
-
-
-Nikita Loh
